edd1d590cb25263616a79d71d93bdad7df39e300
Lucas Gomes de Oliveira <lucas.ggomes@a.ucb.br>
2023-08-09 12:15:14 -0300

    Update verificaNumeroPrimo.js

--- verificaNumeroPrimo.js
+++ verificaNumeroPrimo.js
@@ -1,17 +1,21 @@
-// Função
 function verificarNumeroPrimo(valor) {
   let qtdDiv = 0;
-  if (valor === 1) {
-    // 1 - se o número for primo;
-    return console.log("Resposta = 1");
+  if (valor < 0) {
+    valor *= -1;
+    console.log(valor);
   } else {
-    for (let count = 1; count <= valor; count++) {
-      if ((valor % count) === 0) {
-        qtdDiv++;
-        if (qtdDiv > 2) {
-          // 0 - se o número não for primo;
-          console.log("Resposta = 0");
-          break;
+    if (valor === 1) {
+      // 1 - se o número for primo;
+      return console.log('Resposta = 1');
+    } else {
+      for (let count = 1; count <= valor; count++) {
+        if (valor % count === 0) {
+          qtdDiv++;
+          if (qtdDiv > 2) {
+            // 0 - se o número não for primo;
+            console.log('Resposta = 0');
+            break;
+          }
         }
       }
     }
@@ -19,7 +23,7 @@ function verificarNumeroPrimo(valor) {
 
   if (qtdDiv <= 2) {
     //1 - se o número for primo.
-    console.log("Resposta = 1");
+    console.log('Resposta = 1');
   }
 }
 
