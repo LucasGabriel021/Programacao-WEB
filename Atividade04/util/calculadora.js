4965e7ea80e42db404b1380357a2353d66392f92
Lucas Gabriel Gomes de Oliveira <lucasgabrielgomes33@gmail.com>
2023-09-03 18:14:18 -0300

    Atividade 4

---
+++ Atividade04/util/calculadora.js
@@ -0,0 +1,14 @@
+module.exports = {
+     soma: function(v1, v2) {
+          return v1 + v2;
+     },
+     subtrair: function(v1, v2) {
+          return v1 - v2;
+     },
+     dividir: function(v1, v2) {
+          return v1 / v2;
+     },
+     multiplicar: function(v1, v2) {
+          return v1 * v2;
+     },
+}
