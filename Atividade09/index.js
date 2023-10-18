c1881a07c91b765ee8b20574e853067a1b41f620
LUCAS GABRIEL GOMES DE OLIVEIRA <lucas.ggomes@a.ucb.br>
2023-10-18 10:13:18 -0300

    Arquivos Calculadora

---
+++ Atividade09/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const app = express();
+const PORT = 8080;
+
+app.engine("html", mustacheExpress());
+app.set("view engine", "html");
+app.set("views", __dirname + "/src/views");
+
+app.use(express.urlencoded({extended: true}));
+
+app.use("/", require("./src/routes/calculadoraRoutes"));
+
+app.listen(PORT, () => {
+    console.log("App na porta: ", PORT);
+});
